31db35101cdd82ede207c5ad2ad1f6a53e29b3d3
gregorio aberasturi <gregorioaberasturi@gmail.com>
2023-09-06 20:47:27 -0300

    Commit de menor importancia

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
             <a href="index.html"><img src={logo} alt="Logo" /></a>
             <ul className="navLinks">
                 <li><a href="index.html" className="navbarLink active">Home</a></li>
-                <li><a href="https://www.mercadoagroganadero.com.ar/dll/inicio.dll" target="_blank" rel='noreferrer'>MAG</a></li>
+                <li><a href="https://www.mercadoagroganadero.com.ar/dll/inicio.dll" className="navbarLink" target="_blank" rel='noreferrer'>MAG</a></li>
                 <li><a href="pages/catalogoRemate.html" className="navbarLink">Catálogo</a></li>
                 <li><a href="pages/cereales.html" className="navbarLink">Cereales</a></li>
                 <li><a href="pages/nosotros.html" className="navbarLink">Nosotros</a></li>
