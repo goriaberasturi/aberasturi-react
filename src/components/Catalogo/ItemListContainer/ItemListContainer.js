90ab2df58aadc979cb7fd36f23b5751d646c07e3
gregorio aberasturi <gregorioaberasturi@gmail.com>
2023-09-25 20:48:16 -0300

    Arreglado problema de imagenes luego del filtrado

--- src/components/Catalogo/ItemListContainer/ItemListContainer.js
+++ src/components/Catalogo/ItemListContainer/ItemListContainer.js
@@ -6,12 +6,14 @@ import arrayLotes from '../../../json/lotes.json';
 
 const images = require.context('./../../../images/LoteImgs');
 const imageList = images.keys().map(img => images(img));
-
+arrayLotes.forEach((item, index, array) => {
+    array[index].img = imageList[index];
+});
 
 const ItemListContainer = () => {
 
     const [lotes, setLotes] = useState([]);
-    const {cat} = useParams();
+    const { cat } = useParams();
 
     useEffect(() => {
         const fetchLotes = async () => {
@@ -19,19 +21,16 @@ const ItemListContainer = () => {
                 const data = await new Promise((resolve) => {
                     resolve(cat ? arrayLotes.filter(i => i.categoria === cat) : arrayLotes);
                 })
-                setLotes(data);
-            } catch(error) {
+                setLotes(data)
+                console.log(lotes)
+            } catch (error) {
                 console.log('Error: ', error);
             }
         }
 
-        fetchLotes();
-    }, [cat]);
+        fetchLotes()        
+    }, [cat])
     
-    lotes.forEach((item, index, array) => {
-        array[index].img = imageList[index];
-    });
-
     return (
         <div className='itemListContainer'>
             <h1>NUESTRO CATALOGO DE LOTES</h1>
