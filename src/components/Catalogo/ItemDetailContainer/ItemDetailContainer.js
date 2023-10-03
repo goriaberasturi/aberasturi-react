7453e87269b0813c9b2d2f61ebefe69bfb7c8229
gregorio aberasturi <gregorioaberasturi@gmail.com>
2023-10-02 22:08:18 -0300

    Ya es funcional firebase en el ItemListContainer y ItemDetailContainer. Listo para el merge

--- src/components/Catalogo/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/Catalogo/ItemDetailContainer/ItemDetailContainer.js
@@ -1,40 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ItemDetailContainer.scss';
 import { useParams } from 'react-router-dom';
 import arrayLotes from '../../../json/lotes.json';
 import ItemCount from './ItemCount/ItemCount';
+import { Firestore, doc, getDoc, getFirestore } from 'firebase/firestore';
 
-const images = require.context('./../../../images/LoteImgs');
-const imageList = [];
-
-// Se crea un objeto de la forma {idLote1 : rutaReact1, idLote2 : rutaReact2, ...}
-// Se le quitan a las keys del objeto images los ".", "/" y extension (".jpg") para que quede el id del lote
-images.keys().map(img => {
-    imageList[img.replace('./', '').replace('.jpg', '')] = images(img);
-});
-
-// Se busca el id en el json importado y luego se le asigna a la propiedad imagen, el valor obtenido de buscar el id en el objeto creado en al linea 12
-Object.keys(imageList).forEach(imag => {
-    arrayLotes.find(x => x.id === parseInt(imag)).img = imageList[imag];
-});
 
 const ItemDetailContainer = () => {
 
+    const [lote, setLote] = useState([]);
     const { itemId } = useParams();
 
-    const item = arrayLotes.find(i => String(i.id) === itemId);
+    useEffect(() => {
+        const queryDb = getFirestore();
+        const queryDoc = doc(queryDb, 'lotes', itemId);
+        getDoc(queryDoc)
+            .then((res) => setLote({id: res.id, ...res.data()}));
+    }, [itemId]);
+
+    console.log(lote);
 
     return (
         <div className='ItemDetailContainer'>
             <div className='ItemDetail'>
-                <img src={item.img} alt='cardImg' />
-                <div className='lote-div categoria'>{item.categoria}</div>
-                <div className='lote-div raza'>{item.raza}</div>
-                <div className='lote-div cabezas'><span>Cabezas: </span>{item.cabezas}</div>
-                <div className='lote-div peso'><span>Peso: </span>{item.peso}</div>
-                <div className='lote-div ubicacion'><span>Localidad: </span>{item.ubicacion}</div>
-                <div className='lote-div observaciones'><span>Observaciones: </span>{item.observaciones}</div>
-                <ItemCount id={item.id} stock={item.cabezas} inicial={1}/>
+                <img src={lote.img} alt='cardImg' />
+                <div className='lote-div categoria'>{lote.categoria}</div>
+                <div className='lote-div raza'>{lote.raza}</div>
+                <div className='lote-div cabezas'><span>Cabezas: </span>{lote.cabezas}</div>
+                <div className='lote-div peso'><span>Peso: </span>{lote.peso}</div>
+                <div className='lote-div ubicacion'><span>Localidad: </span>{lote.ubicacion}</div>
+                <div className='lote-div observaciones'><span>Observaciones: </span>{lote.observaciones}</div>
+                <ItemCount id={lote.id} stock={lote.cabezas} inicial={1}/>
             </div>
         </div>
     )
--- src/components/Catalogo/ItemListContainer/ItemListContainer.js
+++ src/components/Catalogo/ItemListContainer/ItemListContainer.js
@@ -2,22 +2,8 @@ import { useState, useEffect } from 'react';
 import Lote from './../Lote/Lote.js';
 import './ItemListContainer.scss';
 import { Link, useParams } from 'react-router-dom';
-import arrayLotes from '../../../json/lotes.json';
+import { getFirestore, collection, getDocs, where, query } from 'firebase/firestore';
 
-// Se obtiene al ruta de react
-const images = require.context('./../../../images/LoteImgs');
-const imageList = [];
-
-// Se crea un objeto de la forma {idLote1 : rutaReact1, idLote2 : rutaReact2, ...}
-// Se le quitan a las keys del objeto images los ".", "/" y extension (".jpg") para que quede el id del lote
-images.keys().map(img => {
-    imageList[img.replace('./', '').replace('.jpg', '')] = images(img);
-});
-
-// Se busca el id en el json importado y luego se le asigna a la propiedad imagen, el valor obtenido de buscar el id en el objeto creado en al linea 12
-Object.keys(imageList).forEach(imag => {
-    arrayLotes.find(x => x.id === parseInt(imag)).img = imageList[imag];
-});
 
 const ItemListContainer = () => {
 
@@ -25,20 +11,30 @@ const ItemListContainer = () => {
     const { cat } = useParams();
 
     useEffect(() => {
-        const fetchLotes = async () => {
-            try {
-                const data = await new Promise((resolve) => {
-                    resolve(cat ? arrayLotes.filter(i => i.categoria === cat) : arrayLotes);
-                })
-                setLotes(data)
-            } catch (error) {
-                console.log('Error: ', error);
-            }
+        const queryDB = getFirestore();
+        const queryCollection = collection(queryDB, 'lotes');
+        if (cat) {
+            const queryFilter = query(queryCollection, where('categoryId', '==', cat));
+            getDocs(queryFilter)
+                .then((res) => setLotes(
+                    res.docs.map((p) => (
+                        { id: p.id, ...p.data() }
+                    )
+                    )
+                )
+                );
+        } else {
+            getDocs(queryCollection)
+                .then((res) => setLotes(
+                    res.docs.map((p) => (
+                        { id: p.id, ...p.data() }
+                    )
+                    )
+                )
+                );
         }
+    }, [cat]);
 
-        fetchLotes()        
-    }, [cat])
-    
     return (
         <div className='itemListContainer'>
             <h1>NUESTRO CATALOGO DE LOTES</h1>
